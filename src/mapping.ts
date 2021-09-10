faf9d30476f1e4a4d7c4aa6161f198452d00d2fe
zendevil <prikshet21@gmail.com>
2021-09-09 21:46:05 -0700

    initial commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,35 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+import { Created } from '../generated/YieldToken/YieldToken'
+import { Raise } from '../generated/schema'
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
-}
+//export function handleNewGravatar(event: NewGravatar): void {
+//  let gravatar = new Gravatar(event.params.id.toHex())
+//  gravatar.owner = event.params.owner
+//  gravatar.displayName = event.params.displayName
+//  gravatar.imageUrl = event.params.imageUrl
+//  gravatar.save()
+//}
+//
+//export function handleUpdatedGravatar(event: UpdatedGravatar): void {
+//  let id = event.params.id.toHex()
+//  let gravatar = Gravatar.load(id)
+//  if (gravatar == null) {
+//    gravatar = new Gravatar(id)
+//  }
+//  gravatar.owner = event.params.owner
+//  gravatar.displayName = event.params.displayName
+//  gravatar.imageUrl = event.params.imageUrl
+//  gravatar.save()
+//}
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
-  }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+export function handleCreated(event: Created): void {
+    let raise = new Raise(event.params.id.toHex())
+    raise.tokenURI = event.params.tokenURI
+    raise.percentage = event.params._percentage
+    raise.minToRaise = event.params._minToRaise
+    raise.bidStart = event.params._bidStart
+    raise.bidEnd = event.params._bidEnd
+    raise.paymentsStart = event.params._paymentsStart
+    raise.paymentsEnd = event.params._paymentsEnd
+    raise.paymentsInterval = event.params._paymentsInterval
+    raise.save()
 }
